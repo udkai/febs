eca5a648613f587c7cf596db976278b1320f9346
udkai <598724366>
2018-08-07 15:50:19 +0800

    第一次提交

---
+++ src/main/java/cc/mrbird/common/aspect/LogAspect.java
@@ -0,0 +1,112 @@
+package cc.mrbird.common.aspect;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import cc.mrbird.common.config.FebsProperies;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.shiro.SecurityUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import cc.mrbird.common.annotation.Log;
+import cc.mrbird.common.util.AddressUtils;
+import cc.mrbird.common.util.HttpContextUtils;
+import cc.mrbird.common.util.IPUtils;
+import cc.mrbird.system.domain.SysLog;
+import cc.mrbird.system.domain.User;
+import cc.mrbird.system.service.LogService;
+
+/**
+ * AOP 记录用户操作日志
+ *
+ * @author MrBird
+ * @link https://mrbird.cc/Spring-Boot-AOP%20log.html
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    @Autowired
+    private FebsProperies febsProperies;
+
+    @Autowired
+    private LogService logService;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Pointcut("@annotation(cc.mrbird.common.annotation.Log)")
+    public void pointcut() {
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint point) throws JsonProcessingException {
+        Object result = null;
+        long beginTime = System.currentTimeMillis();
+        try {
+            // 执行方法
+            result = point.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        // 执行时长(毫秒)
+        long time = System.currentTimeMillis() - beginTime;
+        if (febsProperies.isOpenAopLog())
+            // 保存日志
+            saveLog(point, time);
+        return result;
+    }
+
+    private void saveLog(ProceedingJoinPoint joinPoint, long time) throws JsonProcessingException {
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        SysLog log = new SysLog();
+        Log logAnnotation = method.getAnnotation(Log.class);
+        if (logAnnotation != null) {
+            // 注解上的描述
+            log.setOperation(logAnnotation.value());
+        }
+
+        // 请求的类名
+        String className = joinPoint.getTarget().getClass().getName();
+        // 请求的方法名
+        String methodName = signature.getName();
+        log.setMethod(className + "." + methodName + "()");
+        // 请求的方法参数值
+        Object[] args = joinPoint.getArgs();
+        // 请求的方法参数名称
+        LocalVariableTableParameterNameDiscoverer u = new LocalVariableTableParameterNameDiscoverer();
+        String[] paramNames = u.getParameterNames(method);
+        if (args != null) {
+            StringBuilder params = new StringBuilder();
+            int i = 0;
+            while (i < args.length) {
+                params.append("  ").append(paramNames[i]).append(": ").append(this.mapper.writeValueAsString(args[i]));
+                i++;
+            }
+            log.setParams(params.toString());
+        }
+        // 获取request
+        HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
+        // 设置IP地址
+        log.setIp(IPUtils.getIpAddr(request));
+        log.setUsername(user.getUsername());
+        log.setTime(time);
+        log.setCreateTime(new Date());
+        log.setLocation(AddressUtils.getRealAddressByIP(log.getIp(), mapper));
+        // 保存系统日志
+        this.logService.save(log);
+    }
+}
---
+++ src/main/java/cc/mrbird/common/config/WebConfig.java
@@ -0,0 +1,45 @@
+package cc.mrbird.common.config;
+
+import cc.mrbird.common.xss.XssFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@SuppressWarnings("unchecked")
+public class WebConfig {
+
+    @Autowired
+    private FebsProperies febsProperies;
+
+    /**
+     * XssFilter Bean
+     */
+    @Bean
+    public FilterRegistrationBean xssFilterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new XssFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.setEnabled(true);
+        filterRegistrationBean.addUrlPatterns("/*");
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+        initParameters.put("isIncludeRichText", "true");
+        filterRegistrationBean.setInitParameters(initParameters);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat(febsProperies.getTimeFormat()));
+        return mapper;
+    }
+
+}
